7071a178b8b42c6e6ba3ea1bfe491215ca0398c2
emanlapaz <eugeniojrmanlapaz@gmail.com>
2022-05-26 02:23:42 +0100

    elephantsql

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -7,7 +7,8 @@ import play.test.*;
 import models.*;
 
 @OnApplicationStart
-public class Bootstrap extends Job {
+public class Bootstrap extends Job
+{
     public void doJob() {
         if (Member.count() == 0) {
             Fixtures.loadModels("data.yml");
